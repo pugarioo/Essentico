923c90e693470c64a6c067f8373faf625de4a3e5
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2025-10-16 19:56:02 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
     <Router>
       <div className="App">
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/products" element />
-            <Route path="/about" element />
-            <Route path="/cart" element />
-            <Route path="/profile" element />
-            
-          </Routes>
         </main>
       </div>
     </Router>
