6e643f3935b3388c8ffc11ef0ce8fc5864fb1eb3
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2025-10-23 20:45:18 +0800

    fix product card add to cart and changed product photos

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaBookmark, FaPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom'; 
 import './ProductCard.css';
+import CartContext from '../contexts/CartContext';
 
 function ProductCard({ product }) {
+  const { addToCart } = useContext(CartContext)
+
   if (!product) return null;
 
   const imagePath = require(`../assets/images/${product.image_filename}`);
 
   return (
-    <Link 
-      to={`/products/${product.product_id}`} 
-      className="product-card-link"
-      style={{ textDecoration: 'none', color: 'inherit' }}
-    >
+    
       <div className="product-card">
-        <div className="card-header">
-          <FaBookmark className="bookmark-icon" />
-        </div>
-        <img src={imagePath} alt={product.name} className="product-image" />
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-price">₱ {product.price.toFixed(2)}</p>
-        <div className="add-btn">
+        <Link
+        to={`/products/${product.product_id}`} 
+        className="product-card-link"
+        style={{ textDecoration: 'none', color: 'inherit' }}
+        >
+          <div className="card-header">
+            <FaBookmark className="bookmark-icon" />
+          </div>
+          <img src={imagePath} alt={product.name} className="product-image" />
+          <h3 className="product-name">{product.name}</h3>
+          <p className="product-price">₱ {product.price.toFixed(2)}</p>
+        </Link>
+        <div className="add-btn" onClick={() => addToCart(product)}>
           <FaPlus />
         </div>
       </div>
-    </Link>
   );
 }
 
